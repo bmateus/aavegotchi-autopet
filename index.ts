47b30329e83ddc84b2f88eb87f3b3f227143799c
Bruno Mateus <bmateus@gmail.com>
2021-10-02 23:09:51 -0400

    Added SimpleGotchiPetter Contract

--- index.ts
+++ index.ts
@@ -1,64 +1,55 @@
+require('dotenv').config()
+
 import * as core from "@actions/core";
 import { TransactionResponse } from "@ethersproject/abstract-provider";
 import { ethers } from "ethers";
-
-const TOKEN_IDS = [
-  7988,
-  // <YOUR_GOTCHI_TOKEN_ID_HERE>
-];
 
 const RPC_ENDPOINT = "https://polygon-rpc.com";
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /*
- * Aavegotchi diamond ABI
+ * Simple Aavegotchi Petter ABI
  *
  * https://polygonscan.com/address/0xfa7a3bb12848a7856dd2769cd763310096c053f1
  */
-const abi = ["function interact(uint256[] _tokenIds)"];
+const abi = ["function pet()"];
 
 /*
- * Aavegotchi diamond instance
+ * Simple Aavegotchi Petter Contract Deployment on Polygon
  *
- * https://polygonscan.com/address/0x86935F11C86623deC8a25696E1C19a8659CbF95d
+ * https://polygonscan.com/address/0xa1cc069caa69ced0e0139201c2eb2d6d24c8f711
  */
-const contractAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
+const contractAddress = "0xa1cc069caa69ced0e0139201c2eb2d6d24c8f711";
 
-console.log(`now petting: ${TOKEN_IDS.join(",")}`);
+// make sure this contract has permission to pet your gotchi
+// use the Aavegotchi facet for call 'setPetOperatorForAll'
+// here: https://louper.dev/?address=0x86935F11C86623deC8a25696E1C19a8659CbF95d&network=polygon
+
+console.log(`petting gotchis`);
 
 /*
- * Create Ethers contract instance with signer
- */
+* Create Ethers contract instance with signer
+*/
 const provider = new ethers.providers.JsonRpcProvider(RPC_ENDPOINT);
 
-provider.ready.then(() => {
-  const contract = new ethers.Contract(contractAddress, abi, provider);
-  const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
-  const contractWithSigner = contract.connect(wallet);
+provider.ready.then(async () => {
+  try {
+    const contract = new ethers.Contract(contractAddress, abi, provider);
+    const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+    
+    const gasPrice = await provider.getGasPrice();
 
-  const options = {
-    gasLimit: 88000,
-    gasPrice: ethers.utils.parseUnits("12.0", "gwei"),
-  };
-
-  contractWithSigner
-    .interact(TOKEN_IDS, options)
-    .then((tx: TransactionResponse) => {
-      console.log(`tx sent: ${tx.hash}`);
-      core.setOutput("tx", tx.hash);
-
-      // The operation is NOT complete yet; we must wait until it is mined
-      tx.wait()
-        .then((receipt) => {
-          console.log(`status: ${receipt.status}`);
-        })
-        .catch((e: any) => {
-          console.error(e);
-          throw new Error(e);
-        });
-    })
-    .catch((e: any) => {
-      console.error(e);
-      throw new Error(e);
+    const tx = contract.connect(wallet).pet({
+      gasLimit: 100000,
+      gasPrice: gasPrice,
     });
+
+    const receipt = await tx.wait();
+    console.log(receipt);
+  }
+  catch (e) {
+    console.error(e);
+  }
+
 });
+
